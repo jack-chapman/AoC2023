28f86f5ffed0869efbba11a2b6fbba8d75b49a24
jack-chapman <jackchapman093@gmail.com>
2023-12-04 12:48:16 +0000

    Day 4 part 2

--- day-4/src/main.rs
+++ day-4/src/main.rs
@@ -4,8 +4,10 @@ fn main() {
     let input = include_str!("./input.txt");
 
     let part_1 = process_part_1(input);
+    let part_2 = process_part_2(input);
 
     println!("{part_1}");
+    println!("{part_2}");
 }
 #[derive(Debug)]
 struct Game {
@@ -40,11 +42,7 @@ impl FromStr for Game {
 
 impl Game {
     fn score(self) -> u32 {
-        let winners = self
-            .numbers
-            .iter()
-            .filter(|num| self.winning_numbers.contains(num))
-            .count() as u32;
+        let winners = self.winner_count();
 
         if winners == 0 {
             return 0;
@@ -56,6 +54,21 @@ impl Game {
 
         2_u32.pow(winners - 1)
     }
+
+    fn winner_count(&self) -> u32 {
+        self.numbers
+            .iter()
+            .filter(|num| self.winning_numbers.contains(num))
+            .count() as u32
+    }
+}
+
+fn cards(games: &Vec<u32>, index: u32) -> u32 {
+    let mut result = 0;
+    for i in 0..games[index as usize] {
+        result += cards(games, index + 1 + i)
+    }
+    result + 1
 }
 
 fn process_part_1(input: &str) -> u32 {
@@ -64,6 +77,20 @@ fn process_part_1(input: &str) -> u32 {
         .map(|line| line.parse::<Game>().unwrap())
         .map(|game| game.score())
         .sum()
+}
+
+fn process_part_2(input: &str) -> u32 {
+    let games: Vec<u32> = input
+        .lines()
+        .map(|line| line.parse::<Game>().unwrap().winner_count())
+        .collect();
+    let mut total = 0;
+
+    for (i, _) in games.iter().enumerate() {
+        total += cards(&games, i as u32);
+    }
+
+    total
 }
 
 #[cfg(test)]
@@ -82,5 +109,19 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
         let result = process_part_1(input);
 
         assert_eq!(result, 13);
+    }
+
+    #[test]
+    fn part_2() {
+        let input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+
+        let result = process_part_2(input);
+
+        assert_eq!(result, 30);
     }
 }
