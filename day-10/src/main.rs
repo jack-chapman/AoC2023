d2ef9f6ad20570d2eb81e8fe7116d112e39a7a42
jack-chapman <jackchapman093@gmail.com>
2023-12-12 15:37:11 +0000

    Day 10 Part 1

---
+++ day-10/src/main.rs
@@ -0,0 +1,259 @@
+fn main() {
+    let input = include_str!("./input.txt");
+
+    let part_1 = process_part_1(input);
+
+    println!("{part_1}");
+}
+
+fn process_part_1(input: &str) -> u32 {
+    let grid = make_grid(input);
+
+    let start_position = find_start_position(&grid).expect("Could not find start position");
+    let mut visited_positions: Vec<Position> = vec![];
+
+    let mut current_position = start_position;
+
+    loop {
+        visited_positions.push(current_position.clone());
+
+        let move_candidates = get_valid_neighbours(&current_position, &grid);
+
+        if get_pipe_at_position(&grid, move_candidates.get(0).unwrap()).is_start() {
+            break;
+        }
+
+        let move_candidates: Vec<Position> = move_candidates
+            .into_iter()
+            .filter(|pos| !visited_positions.contains(pos))
+            .collect();
+
+        current_position = move_candidates.get(0).unwrap().clone();
+    }
+
+    (visited_positions.len() / 2).try_into().unwrap()
+}
+
+fn get_valid_neighbours(centre_pos: &Position, grid: &Vec<Vec<Space>>) -> Vec<Position> {
+    let current_pipe = get_pipe_at_position(grid, centre_pos);
+    centre_pos
+        .neighbours()
+        .filter(|(candidate, direction)| {
+            if !position_in_bounds(candidate, grid) {
+                return false;
+            }
+            if let Space::Pipe(pipe) = &grid[candidate.y as usize][candidate.x as usize] {
+                return current_pipe.connects_with(pipe, direction);
+            }
+            false
+        })
+        .map(|a| a.0)
+        .collect()
+}
+
+fn position_in_bounds(position: &Position, grid: &Vec<Vec<Space>>) -> bool {
+    let x = position.x;
+    let y = position.y;
+
+    if y > grid.len() as u32 - 1 {
+        return false;
+    }
+
+    if x > grid[0].len() as u32 - 1 {
+        return false;
+    }
+
+    true
+}
+
+fn get_pipe_at_position(grid: &[Vec<Space>], position: &Position) -> Pipe {
+    if let Space::Pipe(pipe) = &grid[position.y as usize][position.x as usize] {
+        return pipe.clone();
+    }
+    unreachable!()
+}
+
+#[derive(Debug, PartialEq, Clone)]
+enum Direction {
+    North,
+    East,
+    South,
+    West,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+struct Pipe {
+    n: bool,
+    e: bool,
+    s: bool,
+    w: bool,
+}
+
+impl Pipe {
+    fn new(n: bool, e: bool, s: bool, w: bool) -> Self {
+        Self { n, e, s, w }
+    }
+
+    fn connects_with(&self, other: &Self, dir: &Direction) -> bool {
+        match dir {
+            Direction::North => self.n && other.s,
+            Direction::East => self.e && other.w,
+            Direction::South => self.s && other.n,
+            Direction::West => self.w && other.e,
+        }
+    }
+
+    fn is_start(&self) -> bool {
+        self.n && self.e && self.s && self.w
+    }
+}
+
+impl TryFrom<&str> for Pipe {
+    type Error = String;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match value {
+            "|" => Ok(Self::new(true, false, true, false)),
+            "-" => Ok(Self::new(false, true, false, true)),
+            "L" => Ok(Self::new(true, true, false, false)),
+            "J" => Ok(Self::new(true, false, false, true)),
+            "F" => Ok(Self::new(false, true, true, false)),
+            "7" => Ok(Self::new(false, false, true, true)),
+            "S" => Ok(Self::new(true, true, true, true)),
+            other => Err(format!("Cannot convert {other} to Pipe")),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum Space {
+    Pipe(Pipe),
+    Empty,
+}
+
+impl From<&str> for Space {
+    fn from(value: &str) -> Self {
+        match value {
+            "." => Self::Empty,
+            a => match Pipe::try_from(a) {
+                Ok(pipe) => Self::Pipe(pipe),
+                _ => Self::Empty,
+            },
+        }
+    }
+}
+
+fn make_grid(input: &str) -> Vec<Vec<Space>> {
+    input
+        .lines()
+        .map(|line| {
+            line.split("")
+                .filter(|a| !a.is_empty())
+                .map(|a| a.into())
+                .collect()
+        })
+        .collect()
+}
+
+fn find_start_position(grid: &[Vec<Space>]) -> Option<Position> {
+    for (y, row) in grid.iter().enumerate() {
+        for (x, space) in row.iter().enumerate() {
+            if let Space::Pipe(pipe) = space {
+                if pipe.is_start() {
+                    return Some(Position::new(x as u32, y as u32));
+                }
+            }
+        }
+    }
+    None
+}
+
+#[derive(Debug, Clone, PartialEq)]
+struct Position {
+    x: u32,
+    y: u32,
+}
+
+impl Position {
+    fn new(x: u32, y: u32) -> Self {
+        Self { x, y }
+    }
+
+    fn neighbours(&self) -> Neighbours {
+        Neighbours::new(self.clone())
+    }
+}
+
+struct Neighbours {
+    neighbours: Vec<(Position, Direction)>,
+    current_index: usize,
+}
+
+impl Neighbours {
+    fn new(pos: Position) -> Self {
+        let mut neighbours = vec![];
+
+        if pos.y > 0 {
+            neighbours.push((Position::new(pos.x, pos.y - 1), Direction::North));
+        }
+
+        neighbours.push((Position::new(pos.x + 1, pos.y), Direction::East));
+        neighbours.push((Position::new(pos.x, pos.y + 1), Direction::South));
+
+        if pos.x > 0 {
+            neighbours.push((Position::new(pos.x - 1, pos.y), Direction::West));
+        }
+
+        Self {
+            neighbours,
+            current_index: 0,
+        }
+    }
+}
+
+impl Iterator for Neighbours {
+    type Item = (Position, Direction);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current_index >= self.neighbours.len() {
+            return None;
+        }
+
+        let next_pos = &self.neighbours[self.current_index];
+
+        self.current_index += 1;
+
+        Some(next_pos.clone())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_1_a() {
+        let input = ".....
+.S-7.
+.|.|.
+.L-J.
+.....";
+
+        let result = process_part_1(input);
+
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn part_1_b() {
+        let input = "..F7.
+.FJ|.
+SJ.L7
+|F--J
+LJ...";
+
+        let result = process_part_1(input);
+
+        assert_eq!(result, 8);
+    }
+}
